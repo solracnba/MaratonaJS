382e523b69cd8db569ed4fb770295606251f896c
carlos sousa <c.sousa1987@gmail.com>
2020-06-30 18:55:35 -0300

    Link app backend - new account theft implemented

---
+++ links-backend/src/controllers/auth.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bcrypt = require('bcrypt')
+
+const {Account} = require('../models');
+const router = express.Router();
+
+const saltRounds = 10;
+
+router.get('/sign-in', (req, res)=>{
+    return res.json('SING - In');
+})
+
+router.get('/sign-up', async(req, res)=>{
+
+    const {email, password} = req.body;
+
+    const account = await Account.findOne({where:{email}});
+
+    if(account) return res.json('Account already exists');
+
+    const hash = bcrypt.hashSync(password,saltRounds);
+    const newAccount = await Account.create({email, password: hash});
+
+    return res.json({newAccount});
+})
+
+module.exports = router;
+
+
---
+++ links-backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db =require('./models');
+
+const authController = require('./controllers/auth');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/auth', authController);
+
+app.get('/',(req, res)=>{
+    return res.json('Api running');
+});
+
+db.sequelize.sync().then(()=>{
+
+app.listen(3001, function(){
+
+    console.log('Listening on port 3001')
+  });
+});
